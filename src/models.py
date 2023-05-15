55782e45aba8fc26127f51c453c6fbb101645665
BreAnna Baker <124074031+brennybaker@users.noreply.github.com>
2023-05-15 23:54:01 +0000

    Added SQLAlchemy models and generated database relationships

--- src/models.py
+++ src/models.py
@@ -7,26 +7,45 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    addresses = relationship('Address', back_populates='user')
+    blog_posts = relationship('BlogPost', back_populates='user')
+    comments = relationship('Comment', back_populates='user')
 
 class Address(Base):
     __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     street_name = Column(String(250))
     street_number = Column(String(250))
     post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship('User', back_populates='addresses')
+
+class BlogPost(Base):
+    __tablename__ = 'blog_post'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100), nullable=False)
+    content = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship('User', back_populates='blog_posts')
+    comments = relationship('Comment', back_populates='post')
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    content = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship('User', back_populates='comments')
+    post_id = Column(Integer, ForeignKey('blog_post.id'))
+    post = relationship('BlogPost', back_populates='comments')
 
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
+# Draw the ER diagram using SQLAlchemy base
 render_er(Base, 'diagram.png')
